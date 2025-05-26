893caf1fdb8ecd5d514bbf9dbfa7f122d50e4c03
Pablio Richardy Alves Mota <pablio.estag.dti@uniube.br>
2025-05-26 14:23:51 -0300

    fix: alterando conversão do git-bash unix/windows

--- init-dev.sh
+++ init-dev.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+git config --global core.autocrlf false
 
 echo "⏫ Subindo containers..."
 docker-compose build --no-cache
 docker-compose up -d
 
 echo "⚙️ Configurando Git Hooks..."
+
 git config core.hooksPath .git-hooks
 chmod +x .git-hooks/*
 
