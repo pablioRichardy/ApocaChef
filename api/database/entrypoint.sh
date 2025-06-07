bed979f7be63e8addb136ffea1645ce841922fba
Pablio Richardy <yr0p@protonmail.com>
2025-06-07 18:13:10 -0300

    fix: arrumando criação do banco de dados com base no ultimo arquivo e iniciando o banco corretamente

--- api/database/entrypoint.sh
+++ api/database/entrypoint.sh
@@ -3,5 +3,13 @@
 # Inicia cron
 cron
 
-# Chama o entrypoint oficial do Postgres (isso cuida do initdb e restaura backups do /docker-entrypoint-initdb.d)
+# Remove tudo que tem na pasta initdb para garantir limpeza
+rm -f /docker-entrypoint-initdb.d/*.sql
+
+# Copia apenas o arquivo mais recente da pasta backups
+latest_backup=$(ls -t /api/database/backups/*.sql | head -n 1)
+if [ -n "$latest_backup" ]; then
+  cp "$latest_backup" /docker-entrypoint-initdb.d/
+fi
+
 exec docker-entrypoint.sh postgres
